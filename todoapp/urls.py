2b60acfc18829bc5aa7be6021e4a4baa980ac2ed
Olya Ivanova <olya@ivanovs.info>
2017-11-02 16:02:56 +1300

    Todo delete/edit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add$', views.add_todo, name='add_todo'),
     url(r'^todo/(?P<todo_id>\w{0,50})/$', views.todo, name='todo'),
+    url(r'^todo/delete/(?P<todo_id>\w{0,50})/$', views.delete_todo, name='delete_todo'),
+    url(r'^todo/edit/(?P<todo_id>\w{0,50})/$', views.edit_todo, name='edit_todo'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Todo
@@ -26,6 +26,32 @@ def add_todo(request):
     if(request.method == 'POST'):
         title =request.POST['title']
         text =request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+        return redirect('/todoapp')
     else:
         return render(request, 'add_todo.html')
+
+def delete_todo(request, todo_id):
+    if request.method == "POST":
+        todo = Todo.objects.get(id=todo_id)
+        todo.delete()
+        return redirect('/todoapp')
+
+def edit_todo(request, todo_id):
+    if(request.method == 'POST'):
+        title =request.POST['title']
+        text =request.POST['text']
+        todo = Todo.objects.get(id=todo_id)
+        todo.text = text
+        todo.title = title
+        todo.save()
+        return redirect('/todoapp')
+    else:
+        todo = Todo.objects.get(id=todo_id)
+        context = {
+            'todo': todo
+        }
+        return render(request, 'edit_todo.html', context); 
     
